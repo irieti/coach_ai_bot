73c5db953a875ed790d84a15967bdc5258a01c42
irieti <irieid@icloud.com>
2025-05-12 23:13:24 +0100

    lava_url

--- coachbot/urls.py
+++ coachbot/urls.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [path("admin/", admin.site.urls),
-    path(
-        "tinkoff-webhook",
-        views.tinka_webhook,
-        name="tinkoff_webhook",
-    ),
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("webhook/lava/", views.lava_webhook, name="lava_webhook"),
     path("stripe-webhook", views.stripe_webhook, name="stripe_webhook"),
 ]
