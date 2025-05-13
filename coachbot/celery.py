6bf3fad39dc5e307e4409627fb70ea50124dd851
irieti <irieid@icloud.com>
2025-05-13 20:13:22 +0100

    celery task deleted

--- coachbot/celery.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coachbot.settings")
-
-app = Celery("coachbot")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
